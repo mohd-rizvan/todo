a0abfe2472463b1fe818afbe4c06b93cfb82d432
Mohd Rizvan <itsmrizwan@gmail.com>
2014-03-22 03:06:08 +0530

    libraries added to index.html

---
+++ js/application.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: rizvan
+ * Date: 22/3/14
+ * Time: 1:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+window.Todos = Ember.Application.create();
---
+++ js/router.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: rizvan
+ * Date: 22/3/14
+ * Time: 1:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+Todos.Router.map(function() {
+	this.resource('todos', { path: '/' });
+});
